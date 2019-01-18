e197c15fbe3f8e6e73dd632e856d631eb8ca9d7b
Andy Li <andylibc@gmail.com>
2019-01-18 15:26:27 -0800

    add text component

---
+++ app/components/Text/LastConverted.js
@@ -0,0 +1,9 @@
+import React from "react"
+import { Text } from "react-native"
+import moment from "moment"
+
+import styles from "./styles"
+
+const LastConverted = () => {}
+
+export default LastConverted
