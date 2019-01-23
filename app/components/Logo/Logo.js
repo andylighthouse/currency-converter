fbcef93cdb8185ca6943fcd060236e5a647f8c6d
Andy Li <andylibc@gmail.com>
2019-01-23 15:29:38 -0800

    animate logo

--- app/components/Logo/Logo.js
+++ app/components/Logo/Logo.js
@@ -1,11 +1,33 @@
 import React, { Component } from "react"
-import { View, Image, ImageBackground, Text, Keyboard } from "react-native"
+import { View, Image, ImageBackground, Text, Keyboard, Platform } from "react-native"
 
 import background from "./images/background.png"
 import logo from "./images/logo.png"
 import styles from "./styles"
 
 class Logo extends Component {
+  componentDidMount() {
+    let showListener = "keyboardWillShow"
+    let hideListener = "keyboardWillHide"
+
+    if (Platform.OS === "android") {
+      showListener = "keyboardDidShow"
+      hideListener = "keyboardDidHide"
+    }
+    this.keyboardShowListener = Keyboard.addListener(showListener, this.keyboardShow)
+    this.keyboardHideListener = Keyboard.addListener(hideListener, this.keyboardHide)
+  }
+
+  componentWillUnmount() {}
+
+  keyboardShow = () => {
+    console.log("keyboard did show")
+  }
+
+  keyboardHide = () => {
+    console.log("keyboard did hide")
+  }
+
   render() {
     return (
       <View style={styles.container}>
